a0ff990b71dea2314f67d185c2dc546620ae66b7
Frank <sunwu51@126.com>
2019-11-14 22:06:56 +0800

    Update index.js

--- src/http/get-data/index.js
+++ src/http/get-data/index.js
@@ -1,38 +1,27 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+let data = require('@begin/data');
+var option = {
+  table:"test",
+  key:"content"
+}
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
-
-// HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: html
+  var param = req.queryStringParameters;
+  if(param && param.content){
+    await data.set({...option,content:param.content});
+    return {
+      headers: {
+        'content-type': 'text/html; charset=utf8'
+      },
+      body: "ok"
+    }
+  }else{
+    var res = await data.get({...option});
+    return {
+      headers: {
+        'content-type': 'text/html; charset=utf8'
+      },
+      body: JSON.stringify(res)
+    }
+  
   }
 }
